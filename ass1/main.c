fc79a2f5a8ea381603783168d648324954c8e90c
Armandas Rokas <armandas.rokas@gmail.com>
2019-09-11 23:06:45 +0200

    pipe works, but closes after first run. Hence the while(1) does not work propertly

--- ass1/main.c
+++ ass1/main.c
@@ -1,3 +1,13 @@
+/*
+ * Opgave 1:
+ *Finde en måde til ' måske med for loop
+ *som tjekker, hvis det en ' så lave 
+ * mellemrum i stedet
+ *
+ * Opgave 2:
+ * Crashes on empty input
+ */
+
 #include <stdio.h>
 #include <malloc.h>
 #include <sys/types.h>
@@ -10,12 +20,17 @@
 // Executes system calls: fork and execv with
 // given parameters. 
 void execute(char ** args);
+void executePipe(char ** args1, char ** args2);
 
 int main(int argc, char *argv[]){
 	while(1){
 	       	int arg_num = 0;	
 		char input[INPUT_LEN];
 		char * args[MAX_ARGS];
+		char * args1[MAX_ARGS] = {0};
+		char * args2[MAX_ARGS] = {0};
+		int isPipeFound = 0;
+		int pipeIndex;
 		
 		printf("$");
 		fgets(input, INPUT_LEN, stdin);
@@ -32,7 +47,48 @@ int main(int argc, char *argv[]){
 		if(!strcmp(args[0],"exit")){
 			break;
 		}	
-		execute(args);
+		// Search for pipe
+		for(pipeIndex = 0; pipeIndex < arg_num; pipeIndex++){
+			if(strcmp(args[pipeIndex], "|") == 0){
+				isPipeFound = 1;
+				printf("Pipe found \n");
+				break;
+			}
+		}
+
+		if(isPipeFound){
+			printf("Executing pipe");
+			printf("Pipe index %d \n" , pipeIndex);
+			for(int z = 0; z < pipeIndex; z++){
+				args1[z] = args[z];
+				printf("isPipeFound %s", args1[z]);
+			}
+
+			int k=0;
+			for(int d = pipeIndex+1; d < arg_num; d++){
+				args2[k] = args[d];
+				printf("isPipeFound %s ", args2[k]);
+				k++;	
+			}
+			
+			
+			
+			//int status;
+			//pid_t p = fork();
+			//if(p == 0){
+				//child
+				//char * testArgs[] = {"./executePipe", args};
+				//execvp(testArgs[0], testArgs);  
+				executePipe(args1, args2);
+			//} else {
+			//	waitpid(p, &status, 0);
+			//}
+		} else {
+			execute(args);
+		}
+
+
+
 	}
 	
 	return 0;
@@ -54,5 +110,26 @@ void execute(char ** args) {
 			//printf("Done waiting\n");
 		}
 
+}
+
+void executePipe(char ** args1, char ** args2){
+	int status;
+	int fds[2];
+	pipe(fds);
+	int i;
+	pid_t p = fork();
+	
+	if(p==0){ 
+		// child process
+		close(fds[1]);
+		dup2(fds[0],0);
+		execvp(args2[0], args2);
+	} else{
+		// parent process
+		close(fds[0]);
+		dup2(fds[1],1);
+		execvp(args1[0], args1);
+	}
+		
 }
 
