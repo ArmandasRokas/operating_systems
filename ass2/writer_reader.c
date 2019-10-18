63a6b4a03f5584b8fe1f3e36d209795141c44514
armandasrokas <armandas.rokas@gmail.com>
2019-10-18 19:11:36 +0200

    added solution 2

--- ass2/writer_reader.c
+++ ass2/writer_reader.c
@@ -3,193 +3,196 @@
 #include <stdlib.h>
 #include <time.h>
 #define DB_NAME "users.dat"
-#define NUM_USERS 1000000
-#define NUM_USERS_TO_PRINT 30
+#define NUM_USERS_TO_PUT 1000000
+#define NUM_USERS_TO_PRINT 20
 typedef struct user{
 	unsigned int id;
 	char username[20];
 } user;
 
-/**
- * Creates Random Access File with blank users in order to
- * to insert randomly into the file
- */
-void createFileWithBlankUsers(int numOfUsers);
 /** 
- * Initialize users and inserts to users_p array
+ * Initialize users  
  */
 void initUsers(); 
+
 /**
- * Takes an user struct as argument
+ * Takes an address of an user struct as argument
  * and inserts the user into the file
  */
 void putUser(user * userToPut);
+
 /**
  * Prints all users with non-zero id in the file
  */
 void printUsersToConsole();
+
 /**
- * Creates a new thread for every user that needs to be put into the file.
- */
-void executePutUserParallel();
-/**
- * Users is inserted into the file sequentially
- * For testing purposes.
+ * Runs putUser for all users in users array
  */ 
-void *  putRemainingUsers(void * param);
-void putTenUsers();
+void * putUsers(void * param);
+
+/**
+ * Prints users from file.
+ * Uses NUM_USERS_TO_PRINT to define, how many uses should bu printed
+ */
 void * printUsers(void * param);
+
+/**
+ * Puts dummy data into the Random Access file
+ */
 void createFilePutDummyData();
-pthread_t tid[NUM_USERS];
+
+/**
+ * Executes putUsers and printUsers sequentially
+ */
+void executeSequentially();
+
+/**
+ * Executes putUsers and printUsers concurrently, 
+ * but withuot mutex protection.
+ */
+void executeMultiThreading();
+
+/**
+ * Executes putUsers and printUSers concurrently
+ * using with mutex protection.
+ */
+void executeMultiThreadingMutexLock();
+
+
 pthread_mutex_t *lock = NULL;
-//int curr_id = 1;
-user users[NUM_USERS];
-//void * users_p[NUM_USERS];
+user users[NUM_USERS_TO_PUT];
 pthread_t write_thread;
 pthread_t read_thread;
 FILE* fp;
+clock_t start, end_write, end_read;
 
-int main(void)
+int main(int argc, char * argv[])
 {
-	clock_t end_write, end_read;
-	clock_t start = clock();
-	double time_total_write, time_total_read;
-	//setbuf(stdout, NULL);
-	lock = malloc(sizeof(pthread_mutex_t));
-	if (pthread_mutex_init(lock, NULL) != 0){
-			printf("\n Mutex init has failed\n");
-			exit(EXIT_FAILURE);
+	if(argc < 2){
+		printf("Error. No argumentes passed. 1 2 or 3 sould be passed\n");
+		exit(EXIT_FAILURE);
 	}
-	//createFileWithBlankUsers(NUM_USERS+1);
+	
+	double time_total_write, time_total_read;
+	
 	initUsers();
-	
+	printf("Creating dummy data...\n");	
 	createFilePutDummyData();
-	
+	printf("Dummy data is created.\n");
+	start = clock();
 	if((fp = fopen(DB_NAME, "rb+")) == NULL){
 		printf("The file could not be open");		
 	};
-	//putTenUsers(); // put some dummy data into the Random Access file in order to work printTenUsers();
 
-
-	// executeMultiThreading()
-	// executeMultiThreadingWtihMutexLock() // initialize mutex 
-	// executeSequentially()
-
-	//executePutUserParallel();
-	
-	//executePutUserSequentially(); // Uncomment this in order to put user sequentially
-	
-	if( pthread_create(&(write_thread), NULL, putRemainingUsers, NULL)!= 0){
-		printf("Thread can't be created\n");
+	switch(atoi(argv[1])){ // atoi converts string type to integer type
+		case 1:
+			printf("Sequentially mode starts.\n");
+			executeSequentially();
+			break;
+		case 2: 
+			printf("Multi threading mode withuot mutex protection starts.\n");
+			executeMultiThreading();
+			break;
+		case 3:
+			printf("Multi threading mode with mutex protection starts.\n");
+			executeMultiThreadingMutexLock();
+			break;
+		default:
+			printf("Error. Wrong argument entered.\n");
+			exit(EXIT_FAILURE);
+			break;
 	}
-	if( pthread_create(&(read_thread), NULL, printUsers, NULL) != 0){
-		printf("Thread can't be created\n");	
-	}
 	
-	pthread_join(read_thread, NULL);
-	end_read = clock();
-	pthread_join(write_thread, NULL);
-	end_write = clock();
 	time_total_read = ((double) end_read - start);
 	time_total_write = ((double) end_write - start);
 	printf("Read took: %f seconds\n", time_total_read/ CLOCKS_PER_SEC);
 	printf("Write took: %f seconds\n", time_total_write/ CLOCKS_PER_SEC);
 	printf("So read finished %f second earlier than write\n",(time_total_write- time_total_read)/ CLOCKS_PER_SEC );
 	fclose(fp);
-	pthread_mutex_destroy(lock);
-//	printUsersToConsole();
+}
+
+void executeSequentially(){
+        putUsers(NULL);
+        end_write = clock();
+        printUsers(NULL);
+        end_read = clock();
 }
 
 
-void createFileWithBlankUsers(int numOfUsers){
-	FILE * fPtr;  // file pointer	
-	unsigned int i;
-	user blankUser = {0, ""};
-	if((fPtr = fopen(DB_NAME, "wb")) == NULL)
-	{
-		printf("File could not be opened.");
-	} else
-	{
-		for(i = 0; i < numOfUsers; i++)
-		{
-			fseek(fPtr, i*sizeof(user), SEEK_SET);
-			fwrite(&blankUser, sizeof(user), 1, fPtr);	
-		}
-		fclose(fPtr);
-	}
+void executeMultiThreading(){
+	if( pthread_create(&(write_thread), NULL, putUsers, NULL)!= 0){
+                printf("Thread can't be created\n");
+        }
+        if( pthread_create(&(read_thread), NULL, printUsers, NULL) != 0){
+                printf("Thread can't be created\n");
+        }
+
+        pthread_join(read_thread, NULL);
+        end_read = clock();
+        pthread_join(write_thread, NULL);
+        end_write = clock();
+
+}
+
+void executeMultiThreadingMutexLock(){
+        lock = malloc(sizeof(pthread_mutex_t));
+        if (pthread_mutex_init(lock, NULL) != 0){
+                        printf("\n Mutex init has failed\n");
+                        exit(EXIT_FAILURE);
+        }
+       
+       
+	if( pthread_create(&(write_thread), NULL, putUsers, NULL)!= 0){
+                printf("Thread can't be created\n");
+        }
+        if( pthread_create(&(read_thread), NULL, printUsers, NULL) != 0){
+                printf("Thread can't be created\n");
+        }
+
+        pthread_join(read_thread, NULL);
+        end_read = clock();
+        pthread_join(write_thread, NULL);
+        end_write = clock();
+	
+	pthread_mutex_destroy(lock);
 }
 
 void initUsers(){
-	// set usernames that starts from 1 to NUM_USERS+1
-	for(int c = 1; c < NUM_USERS+1; c++){
+	// set id and usernames that starts from 1 to NUM_USERS+1
+	for(int c = 1; c < NUM_USERS_TO_PUT+1; c++){
 		users[c-1].id = c;
 		sprintf(users[c-1].username, "username%d", c);
 	}
-	// initializes users pointer array 
-	//for(int j = 0; j < NUM_USERS; j++){
-	//	users_p[j] = &users[j];
-	//}	
 }
 
 void putUser(user * userToPut){
-	if(lock != NULL){
-		//pthread_mutex_lock(lock);
-	}
-//	FILE * fp;   // users.dat file pointer
 	
 	if( fp  == NULL)
 	{
 		printf("File could not be opened.");
-	} else{ 
-		//userToPut->id = curr_id;
+	} else{
+	       	// lock	
+	        if(lock != NULL){
+	                pthread_mutex_lock(lock);
+       		 }
+
 		fseek(fp, ( userToPut->id - 1) * sizeof(user), SEEK_SET);
 		fwrite(userToPut, sizeof(user), 1, fp);
-		//curr_id++;
-	}
-//	fclose(fp);
-	if(lock != NULL){
-		//pthread_mutex_unlock(lock);
+		// unclock
+		if(lock != NULL){
+	                pthread_mutex_unlock(lock);
+        	}
 	}
 }
-
-void putTenUsers(){
-	int i = 0;
-	while(i<10){
-		putUser(&users[i]);
-		i++;
-	}	
-}
-/*
-void executePutUserParallel(){
-	lock = malloc(sizeof(pthread_mutex_t));
-	if (pthread_mutex_init(lock, NULL) != 0){
-			printf("\n Mutex init has failed\n");
-			exit(EXIT_FAILURE);
-	}
-
-	int i = 0;
-	int error;
-	while(i<NUM_USERS){
-		error = pthread_create(&(tid[i]), NULL, putUser, users_p[i]);
-		if(error != 0){
-			printf("Thread can't be created\n");
-		}
-		i++;
-	}
-	for(int d = 0; d < NUM_USERS; d++){
-		pthread_join(tid[d],NULL);
-	}
-	pthread_mutex_destroy(lock);
-}*/
 void createFilePutDummyData(){
-	//FILE * fPtr;  // file pointer   
         int i = 0;
         if((fp = fopen(DB_NAME, "wb")) == NULL)
         {
                 printf("File could not be opened.");
         } else
         {
-                while(i < NUM_USERS){
+                while(i < NUM_USERS_TO_PUT){
 			putUser(&users[i]);
 			i++;
 		}
@@ -198,61 +201,40 @@ void createFilePutDummyData(){
 
 }
 
-void * putRemainingUsers(void * param){ // make a thread. to execute all users. Mutex lock only atomic in the file. 
-	int i = 10;
-	while(i<NUM_USERS){
-	//	printf(".");
-	//	fflush(stdout);
+void * putUsers(void * param){  
+	int i = 0;
+	while(i<NUM_USERS_TO_PUT){
 		putUser(&users[i]);
 		i++;
 	}
 }
 void * printUsers(void * param){
-//	pthread_mutex_lock(lock);
-//	FILE * fp;   // users.dat file pointer
-	
-//	if(( fp = fopen(DB_NAME, "rb+")) == NULL)
-//	{
-//		printf("File could not be opened.");
-//	} else{ 
-
-	fseek(fp,0, SEEK_SET);
 	user blankUser = {0, ""};
 	
-	printf("%-6s%-16s\n", "Id", "User name");
-	//fflush(stdout);
-	int i = 0;
-	while(i<NUM_USERS_TO_PRINT){
-		fseek(fp,i*sizeof(user), SEEK_SET);
-		fread(&blankUser, sizeof(user), 1, fp);
-		if(blankUser.id != 0){
-			printf("%-6d%-16s\n", blankUser.id, blankUser.username);
-	//		fflush(stdout);
-		}
-		i++;
-	}
-//		fclose(fp);
-//	pthread_mutex_unlock(lock);
-//	}
-}
-
-void printUsersToConsole(){
-	FILE * fp;
-	fseek(fp, 0, SEEK_SET);
-	user blankUser = {0, ""};/*
-	if(( fp = fopen(DB_NAME, "rb")) == NULL)
+	if(fp  == NULL)
 	{
-		printf("File could not be opened. \n");
-	}*/
-	printf("%-6s%-16s\n", "Id", "User name");
+		printf("File could not be opened.");
+	} else{ 
+		printf("%-6s%-16s\n", "Id", "User name");
+		int i = 0;
+		while(i<NUM_USERS_TO_PRINT){
+			// lock 
+			if(lock != NULL){
+				pthread_mutex_lock(lock);
+			}
 
-	while(!feof(fp)){
-		fread(&blankUser, sizeof(user), 1, fp);
-		if(blankUser.id != 0){
-			printf("%-6d%-16s\n", blankUser.id, blankUser.username);
+			fseek(fp,i*sizeof(user), SEEK_SET);
+			fread(&blankUser, sizeof(user), 1, fp);
+			// unlock 
+			if(lock != NULL){
+				pthread_mutex_unlock(lock);
+			}
+
+			if(blankUser.id != 0){
+				printf("%-6d%-16s\n", blankUser.id, blankUser.username);
+			}
+			i++;
 		}
 	}
-	//fflush(stdin);
-	//fclose(fp);
 }
 
